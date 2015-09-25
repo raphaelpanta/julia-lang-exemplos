0f7f1b6205ff59a6a73d89ae76838c4b05ddba3a
Raphael Pantaleão <raphaelpanta@gmail.com>
2015-09-25 12:09:54 -0300

    Refatoração com mapreduce

--- julia-lang-exemplos/src/xadrez.jl
+++ julia-lang-exemplos/src/xadrez.jl
@@ -1,18 +1,5 @@
 module Xadrez end
 
-function xadrez(n, m)
-  x = ""
-  if n == 0 || m == 0
-    return x
-  end
-  for i = 1:m
-    for j = 1:n
-      x = x * string((i + j -2)%2)
-    end
-      x = x * "\n"
-  end
-  return x
-end
-
+xadrez(n, m) = n == 0 || m == 0 ? "" : mapreduce ( z-> mapreduce(y -> string((z + y -2) % 2), * , [1:n]) * "\n", * , [1:m])
 
 export xadrez
